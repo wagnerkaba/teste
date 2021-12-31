4016eab2466ca41ca010c130f6dfeb7d757ffd88
wagnerkaba <bom.link+github@gmail.com>
2021-12-31 14:55:23 -0300

    Revert "first commit"
    This reverts commit 21b6d211460431b2304869070abf2db1126d362a.

--- src/main/java/com/wagner/tqi/auth/ApplicationUserDao.java
+++
@@ -1,8 +0,0 @@
-package com.wagner.tqi.auth;
-
-import java.util.Optional;
-
-public interface ApplicationUserDao {
-
-    public Optional<ApplicationUser> selectApplicationUserByUsername(String username);
-}
